e3f042fabb9d638f956c3e8b98f6823dec5aacbf
S0rbex <vitalikmartunushun@gmail.com>
2026-04-02 13:14:03 +0300

    Update src/main/java/vitalitus/springtestproject/util/JwtAuthenticationFilter.java
    Co-authored-by: Olexandr Fetisov <149307126+fetis-off@users.noreply.github.com>

--- src/main/java/vitalitus/springtestproject/util/JwtAuthenticationFilter.java
+++ src/main/java/vitalitus/springtestproject/util/JwtAuthenticationFilter.java
@@ -43,7 +43,7 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
     }
 
     private String getToken(HttpServletRequest request) {
-        String bearerToken = request.getHeader("Authorization");
+        String bearerToken = request.getHeader(HttpHeaders.AUTHORIZATION);
         if (StringUtils.hasText(bearerToken) && bearerToken.startsWith("Bearer ")) {
             return bearerToken.substring(7);
         }
