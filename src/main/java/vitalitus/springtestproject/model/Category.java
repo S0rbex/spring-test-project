52050423b6bd3c82ee81e8e6941ff43f2bdb0d45
S0rbex <vitalikmartunushun@gmail.com>
2026-04-15 00:25:48 +0300

    fixing comments

--- src/main/java/vitalitus/springtestproject/model/Category.java
+++ src/main/java/vitalitus/springtestproject/model/Category.java
@@ -7,17 +7,16 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 import lombok.Setter;
 import org.hibernate.annotations.SQLDelete;
-import org.hibernate.annotations.Where;
+import org.hibernate.annotations.SQLRestriction;
 
 @Entity
 @Getter
 @Setter
 @Table(name = "categories")
 @SQLDelete(sql = "UPDATE categories SET is_deleted = true WHERE id = ?")
-@Where(clause = "is_deleted = false")
+@SQLRestriction(value = "is_deleted = false")
 public class Category {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
